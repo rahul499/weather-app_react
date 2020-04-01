886d2ceab16b18281ba656528bf11bbebae87356
rahul499 <55375819+rahul499@users.noreply.github.com>
2020-04-01 17:14:10 +0530

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Form extends React.Component{
+
+	render(){
+		return(
+		<form onSubmit={this.props.getWeather} >
+					<br />
+         <input type="text" name="city" placeholder="City..." />
+         <input type="text" name="country" placeholder="Country..." />
+         <button>Get Weather</button>
+         <br /><br />
+         </form>
+
+         );
+	}
+}
+
+export default Form;
