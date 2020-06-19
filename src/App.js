2c0f8dafa5ec7af45bd07bceaceba28877cba635
rahul499 <rahul4mishra99@gmail.com>
2020-06-20 01:40:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,94 +1,19 @@
-import React from 'react';
-import a from './images/weather.jpg';
-import './App.css';
-import Weather from './components/Weather.js';
-import Form from './components/Form.js';
+import React from "react"
 
-
-const API_KEY ="9882fbb7c3586cdcea57be629af28c82";
-
-class App extends React.Component{
-
-state={
-
-  temperature: undefined,
-
-  city: undefined,
-
-  country: undefined,
-
-  humidity: undefined,
-
-  description: undefined,
-
-  error: undefined
+import Wrapper from "./components/Basic/Wrapper"
+import Main from "./components/Main"
+ 
+const App = () => {
+  return (
+    <Wrapper>
+      <Main />
+    </Wrapper>
+  )
 }
 
-getWeather = async (e) => {
-  e.preventDefault();
-  const city =e.target.elements.city.value;
-   const country =e.target.elements.country.value;
+export default App
 
 
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); 
 
-      const data = await api_call.json();
 
-if(city && country)
-  {
-      console.log(data);
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: " "
-      })
-  }
-else
-  {
-      this.setState({
-         temperature: undefined,
 
-         city: undefined,
-
-         country: undefined,
-
-         humidity: undefined,
-
-         description: undefined,
-
-        error: "Please enter the values."
-      })
-  }
-}
-
-  render(){
-    return (
-    <div className="App">
-    <div className="App1">
-     <center>
-        <img src={a} className="App-logo" alt="logo" /> </center>
-         <b> Weather Master</b><br/>
-         </div>
-      <header className="App-header">
-      
-          <Form getWeather={this.getWeather} />
-
-             <Weather 
-             temperature= {this.state.temperature}
-             city = {this.state.city}
-             country= {this.state.country}
-             humidity= {this.state.humidity}
-             description= {this.state.description}
-             error= {this.state.error}
-             />
-      </header>
-
-    </div>
-  );
-}
-} 
-
-export default App;
