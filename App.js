787585f038f5a1c1070ac9801304971f89458092
rahul499 <rahul4mishra99@gmail.com>
2020-06-20 00:34:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Wrapper from "./components/Basic/Wrapper"
+import Main from "./components/Main"
+ 
+const App = () => {
+  return (
+    <Wrapper>
+      <Main />
+    </Wrapper>
+  )
+}
+
+export default App
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./styles/styles.scss"
+import App from "./App"
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App/>, document.getElementById("root"))
+
+serviceWorker.unregister();
