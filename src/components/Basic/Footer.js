e980efadc8150e2346411ec8218f25c170bd222d
rahul499 <rahul4mishra99@gmail.com>
2020-06-20 01:44:19 +0530

    Add files via upload

---
+++ src/components/Basic/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+  <div className="footer">
+    <p>Developed by: <a rel="noopener noreferrer" target="_blank" href="https://www.github.com/rahul499/weather-app_react">@Rahul</a></p>
+  </div>
+)
+
+export default Footer
